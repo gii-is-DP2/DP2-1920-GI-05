698def9d257b12ffd88e0448969f79785ff5b92e
GuillermoDP14 <guigarjim@alum.us.es>
2020-03-16 00:23:33 +0100

    Añadido CategoryFormatter
    Referencia: #5 Para poder mostrar en el sistema correctamente las categorias de los torneos.

--- src/main/java/org/springframework/samples/petclinic/repository/springdatajpa/SpringDataTournamentRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/springdatajpa/SpringDataTournamentRepository.java
@@ -6,12 +6,12 @@ import org.junit.experimental.categories.Categories;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.samples.petclinic.model.Category;
 import org.springframework.samples.petclinic.model.Tournament;
 import org.springframework.samples.petclinic.repository.TournamentRepository;
 
 public interface SpringDataTournamentRepository extends TournamentRepository, Repository<Tournament, String>{
 
-	
 	
 	
 }
--- src/main/java/org/springframework/samples/petclinic/service/TournamentService.java
+++ src/main/java/org/springframework/samples/petclinic/service/TournamentService.java
@@ -1,5 +1,6 @@
 package org.springframework.samples.petclinic.service;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.samples.petclinic.model.Tournament;
@@ -16,8 +17,7 @@ public class TournamentService {
 	public TournamentService(TournamentRepository tournamentRepository) {
 		this.tournamentRepository = tournamentRepository;
 	}
-	
-	
+		
 	@Transactional
 	public void saveTournament(Tournament tournament) throws DataAccessException {
 		tournamentRepository.save(tournament);
