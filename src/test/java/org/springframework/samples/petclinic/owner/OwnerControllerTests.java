5f174029f55bced251231f663b911e0f12ac68f3
Joaquin Gonzalez <joagonrui1@alum.us.es>
2020-04-24 17:21:39 +0200

    OwnerControllerE2E Test

--- src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerTests.java
@@ -155,7 +155,7 @@ class OwnerControllerTests {
 		mockMvc.perform(post("/owners/{ownerId}/edit", TEST_OWNER_ID).with(csrf()).param("firstName", "Joe")
 				.param("lastName", "Bloggs").param("address", "123 Caramel Street").param("city", "London")
 				.param("telephone", "01616291589")).andExpect(status().is3xxRedirection())
-				.andExpect(view().name("redirect:/owners/details"));
+				.andExpect(view().name("owners/details"));
 	}
 
 	@WithMockUser(value = "spring")
