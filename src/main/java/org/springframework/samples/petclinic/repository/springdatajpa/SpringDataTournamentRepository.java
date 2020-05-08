55b6d465551439412716be19e781e5ba09072f0a
GuillermoDP14 <guigarjim@alum.us.es>
2020-05-08 12:13:19 +0200

    prueba

--- src/main/java/org/springframework/samples/petclinic/repository/springdatajpa/SpringDataTournamentRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/springdatajpa/SpringDataTournamentRepository.java
@@ -1,15 +1,12 @@
 package org.springframework.samples.petclinic.repository.springdatajpa;
 
 import java.util.Collection;
-import java.util.List;
 
-import org.junit.experimental.categories.Categories;
+
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.samples.petclinic.model.Category;
-import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.model.Tournament;
 import org.springframework.samples.petclinic.repository.TournamentRepository;
 
