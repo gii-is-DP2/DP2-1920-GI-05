1a32c6a743878c125dbcef7ebdfd0a080bb8fabc
rayo329 <rayo329@hotmail.com>
2020-05-26 23:37:40 +0200

    fixed ranking and tests made

---
+++ src/test/java/org/springframework/samples/petclinic/ranking/RankingControllerE2ETests.java
@@ -0,0 +1,52 @@
+package org.springframework.samples.petclinic.ranking;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(
+  webEnvironment=SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@Transactional
+class RankingControllerE2ETests {
+
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+
+	@WithMockUser(username="owner1",authorities= {"owner"})
+	@Test	
+	void testListAll() throws Exception {
+		mockMvc.perform(get("/rankings/all")).andExpect(status().isOk())
+				.andExpect(model().attributeExists("rankings")).andExpect(view().name("rankings/list"));
+	}
+
+		
+
+	  
+
+
+
+
+	
+
+	
+	
+
+	 
+}
